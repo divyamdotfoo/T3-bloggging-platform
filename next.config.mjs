ef47c03ea3ac10efe94c51beb014016c1a36eaca
divyam gupta <divyam7091@gmail.com>
2024-01-19 23:24:50 +0530

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
 
-export default nextConfig;
+/** @type {import("next").NextConfig} */
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+        protocol: "https",
+      },
+      {
+        hostname: "utfs.io",
+        pathname: "/**",
+        protocol: "https",
+      },
+    ],
+  },
+};
+
+export default config;
