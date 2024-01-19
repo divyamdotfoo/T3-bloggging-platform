4a1382ea7e745fe407397b4e20b00fa0dfdd0bf9
divyam gupta <divyam7091@gmail.com>
2024-01-19 21:42:44 +0530

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,11 @@ import { api } from "@/server/react";
 
 export default function Page() {
   const users = api.user.getMany.useQuery({ take: 2 });
-  const draft = api.user.checkDraft.useQuery({});
   return (
     <div>
       {users.data?.map((d) => (
         <p key={d.name}>{d.name}</p>
       ))}
-      <p>{draft.isError ? "error" : "why not error"}</p>
     </div>
   );
 }
